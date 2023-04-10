a3c437ae4cefafaadc08a0a28e4af4285e4da4af
Kanachi Weli <101941959+Kanachiw@users.noreply.github.com>
2023-04-10 13:37:54 -0500

    Update letterGrid.cpp

--- letterGrid.cpp
+++ letterGrid.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string> 
 #include "d_matrix.h"
-
 #include <fstream>
 using namespace std;  
 
@@ -90,8 +89,5 @@ void LetterGrid::printLetters(){
     }
     
 }
-=======
-using namespace std; 
-/*Implement a class called LetterGrid that reads the letters in the grid from a file and stores them in a matrix.
-     (a) Also provide a function for this class that can print out the n x n grid of letters read from the file*/
+
 
